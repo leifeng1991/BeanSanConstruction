3ee6fba8473d9c555b51970f253c36d3184b6bf0
leifeng <helf@moutaiclub.org>
2022-04-22 14:35:48 +0800

    修改操作权限和地图定位

--- app/src/main/java/com/trinidad/beansanconstruction/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/trinidad/beansanconstruction/ui/fragment/HomeFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import com.moufans.lib_base.base.fragment.ViewPageFragment
 import com.moufans.lib_base.utils.StatusBarUtil
+import com.moufans.lib_base.utils.ToastUtil
 import com.trinidad.beansanconstruction.R
+import com.trinidad.beansanconstruction.constants.AppConstants
 import com.trinidad.beansanconstruction.databinding.FragmentNewHomeBinding
 import com.trinidad.beansanconstruction.ui.activity.ProjectManagementActivity
 import com.trinidad.beansanconstruction.ui.activity.RefuelManagementActivity
@@ -12,6 +14,7 @@ import com.trinidad.beansanconstruction.ui.activity.RichScanActivity
 import com.trinidad.beansanconstruction.ui.adapter.HomeBannerAdapter
 import com.trinidad.beansanconstruction.ui.adapter.HomeFeatureListAdapter
 import com.trinidad.beansanconstruction.ui.bean.FeatureListBean
+import com.trinidad.beansanconstruction.utils.SharedPrefUtil
 import com.youth.banner.indicator.RectangleIndicator
 
 
@@ -53,6 +56,10 @@ class HomeFragment : ViewPageFragment<FragmentNewHomeBinding>() {
         // 功能列表点击事件
         mHomeFeatureListAdapter.setOnItemClickListener { _, _, position ->
             val title = mHomeFeatureListAdapter.data[position].title
+            if (SharedPrefUtil.get(AppConstants.USER_CODE, "") == "YSSJ" && title != "车辆维修") {
+                ToastUtil.showShort("请向管理员开通权限")
+                return@setOnItemClickListener
+            }
             when (title) {
                 // 加油
                 "加油" -> {
